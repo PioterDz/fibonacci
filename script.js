9b953b74607c13f54bb80077bc25515a04d3da34
Piotr Dz <dojlidy.zubr@gmail.com>
2021-08-24 22:20:52 +0200

    poprawa funkcji fibonacciego

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-function fibonacci(startingNumber) {
-    function calc(arr) {
-        const sumUp = arr[arr.length - 2] + arr[arr.length - 1];
-        if(sumUp === startingNumber) {
-            return arr;
-        } else if(sumUp > startingNumber) {
-            throw Error('Podano numer nienależący do ciągu Fibonnaciego');
-        } else {
-            arr.push(sumUp);
-            return calc(arr);
-        }
-    }
-    return startingNumber === 0 ? [0] : calc([0,1]);
-}
-
 function nwd(a, b) {
     if(a === b) {
         return a;
     } else {
         return a > b ? nwd(a - b, b) : nwd(a, b - a);
     }
+}
+
+function fibonacci(numberToShow) {
+    const startingArray = [0,1];
+    if(numberToShow == 0) return null;
+    if(numberToShow == 1) return [...startingArray[0]];
+    if(numberToShow == 2) return [...startingArray];
+    
+    for(let i = 2; i < numberToShow; i++) {
+        // sposób nr 1
+        const newValue = startingArray[startingArray.length - 2] + startingArray[startingArray.length - 1];
+        // sposób nr 2 (wykomentowany, żeby działało bo 2x const)
+        // const newValue = startingArray.slice(startingArray.length - 2).reduce((acc, curr) => acc + curr);
+        startingArray.push(newValue);
+    }
+
+    return startingArray;
 }
